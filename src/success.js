3d34e78f5c3206fcf20c24bb92503a973f363e66
amin <i400406@fhict.nl>
2021-09-30 11:18:55 +0200

    secondary commit

--- src/success.js
+++ src/success.js
@@ -3,7 +3,7 @@ import { Context } from './store'
 import './App.css';
 import {
     Card, CardText, CardBody,
-    CardTitle, CardSubtitle, CardHeader, Breadcrumb, BreadcrumbItem
+    CardTitle, CardSubtitle, CardHeader, Breadcrumb, BreadcrumbItem, Container
 } from 'reactstrap';
 
 const Success = () => {
@@ -20,14 +20,15 @@ const Success = () => {
                     <CardHeader>Success</CardHeader>
                 </BreadcrumbItem>
             </Breadcrumb>
-            <Card className="text-center" style={{ backgroundColor: '#74BDCB', borderColor: '#658EA9', height: "40vh", marginTop: "5vh", color: "#658EA9" }}>
-
-                <CardBody class="card-body">
-                    <CardTitle tag="h2"><a href={"mailto:" + state.email} style={{ color: "#658EA9" }} >{state.fName.toUpperCase()}  {state.lName.toUpperCase()}</a></CardTitle>
-                    <CardSubtitle tag="h3" className="mb-2 text-muted">Age: {age}</CardSubtitle>
-                    <CardText class="card-subtitle mb-2 text-muted"> <a class="card-link" href={state.phone}>Call {state.phone}</a></CardText>
-                </CardBody>
-            </Card>
+            <Container className="text-center" >
+                <Card border="dark" style={{ backgroundColor: '#74BDCB', borderColor: '#658EA9', height: "40vh", marginTop: "5vh", color: "#658EA9", width: "50vw", marginLeft: "25vw", marginRight: "25vw" }}>
+                    <CardBody class="card-body">
+                        <CardTitle tag="h2"><a href={"mailto:" + state.email} style={{ color: "#658EA9" }} >{state.fName.toUpperCase()}  {state.lName.toUpperCase()}</a></CardTitle>
+                        <CardSubtitle tag="h3" className="mb-2 text-muted">Age: {age}</CardSubtitle>
+                        <CardText class="card-subtitle mb-2 text-muted"> <a class="card-link" href={state.phone}>Call {state.phone}</a></CardText>
+                    </CardBody>
+                </Card>
+            </Container>
         </>
     )
 }
